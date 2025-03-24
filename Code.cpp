62576393162d4dc57f49919ef21b700397b5322b
Vladislav <vlad_fedorov2004@mail.ru>
2025-03-24 07:29:11 +0700

    Commit Code Petya

---
+++ Code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int i = 0;
+	int count = 0;
+	string line[20];
+	cout << "enter 20 letters";
+	for (i = 0; i < 20; i++){
+		cin >> line[i];
+		if (line[i] <= && line [i] >=){
+			count ++;
+		}else{
+			if(line[i] <= && line[i] >=){
+				count ++;
+			}
+		}
+	}
+	cout << "\ncount of high register lettes: " << count << endl;
+	
+	
+}
